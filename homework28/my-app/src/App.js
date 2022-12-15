06ca844f6a1b6eef3b2c42e4373182e818232bd7
alena-vovkohon <alena.vovkogon@gmail.com>
2022-12-15 07:50:01 -0500

    homework28

--- homework28/my-app/src/App.js
+++ homework28/my-app/src/App.js
@@ -1,24 +1,64 @@
 import React from 'react';
+import { Component } from 'react';
 import './App.css';
 import Head from './Head/Head';
 import Photo from './Main/Photo'
 import Banner from './Banner/Banner'
 
-function App() {
-  return (
-    <div className="App">
-      <Head />
-      <div className ='Section'>
-        <Banner title={'Title'} article={'your advertisement can be here'} />
-        <div className="Main">
-          <Photo name={'Ski'}  description ={'winter'} year={'2021'} />
-          <Photo name={'Mountans'}  description ={'fall'} year={'2020'} />
-          <Photo name={'Sea'} description={'summer'} year={'2019'} /> 
-          <Photo name={ 'Flowers' } description ={'spring'} year ={'2020'} /> 
+class App extends Component {
+  constructor() {
+  super();
+    this.state = {
+      value: '',
+      title: [
+        {
+        name: 'Ski',
+        description: 'winter',
+        year: '2021'
+        },
+        {
+        name: 'Mountans',
+        description: 'fall',
+        year: '2020'
+        },
+        {name: 'Sea',
+        description: 'summer',
+        year: '2019'
+        },
+        {name: 'Flowers',
+        description: 'spring',
+        year: '2020'
+        },
+      ]
+    }
+  } 
+  
+  handlerChanger = (value) => {
+    this.setState({...this.state, value})
+  }
+
+  handlerFilter = (value) => {
+    let resaltFilter = this.state.title.filter(item => item.description.toLowerCase().includes(value.toLowerCase()))
+    this.setState({ ...this.state, title:resaltFilter}) 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Head onChange={this.handlerChanger} onFilter={this.handlerFilter} />
+        <div className='Section'>
+          <Banner title={'Title'} article={'your advertisement can be here'} />
+          <div className="Main">
+            {this.state.title.map((item, index) => {
+              return (
+                <Photo key={index} name={item.name} description={item.description} year={item.year} />
+              )
+            })}
+          </div>
         </div>
-      </div> 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
--- homework28/my-app/src/Banner/Banner.js
+++ homework28/my-app/src/Banner/Banner.js
@@ -4,15 +4,31 @@ import './Banner.css'
 
 class Banner extends Component {
     constructor(props) {
-      console.log(props)
+      // console.log(props)
       super(props);
+      this.state = {
+        isOpen: false
+      }
+
     }
+  
+  handlerOpen = () => {
+    this.setState({...this.state, isOpen:!this.state.isOpen})
+  }
+  
 
   render() {
     return (
       <div className='Banner'>
-            <h3> {this.props.title}</h3>
-            <p> {this.props.article}</p> 
+        <h3> {this.props.title}</h3>
+        <p> {this.props.article}</p> 
+        <button onClick={this.handlerOpen}>Price</button>
+        {
+          this.state.isOpen ? 
+          (<p> contacting us email qwertyuio@gmail.com or calling 123-4567</p>) :
+          null
+        }
+        
       </div>
      
     );
